5169028b2034bc5d06077bb7e5f443ee6ed7720c
Mark Choi <fantasysa@gmail.com>
2020-04-06 23:39:42 +0900

    Migrate some views to Loading view.

--- Loading.js
+++ Loading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-function Loading() {
+export default function Loading() {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
