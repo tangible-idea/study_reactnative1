f97d426c385c6952ec64e9144bae554076265532
Mark Choi <fantasysa@gmail.com>
2020-04-06 23:51:43 +0900

    get latitude and longitude.

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ import * as Location from 'expo-location';
 export default class extends React.Component{
   getLocation= async() => {
     try {
+
       const response= await Location.requestPermissionsAsync();
       console.log(response);      
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const {coords} = await Location.getCurrentPositionAsync();
+      console.log(coords.latitude, coords.longitude);
+      
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
