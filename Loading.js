d4ef7e50b4f474d17a0cd491e45097885214a0ff
Mark Choi <fantasysa@gmail.com>
2020-04-09 01:51:08 +0900

    add ActivityIndicator

--- Loading.js
+++ Loading.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default function Loading() {
     return (
         <View style={styles.container}>
         <StatusBar barStyle="light-content"/>
-
+          <ActivityIndicator size="large" color="#333333" style={styles.spinner} />
             <Text>Open up App.js to start working on your app!</Text>
             <Text style={styles.text}>Getting the fucking weather</Text>
         </View>
@@ -22,6 +22,11 @@ const styles = StyleSheet.create({
     text: {
       color: "#2c2c2c",
       fontSize:30 
+    },
+    spinner: {
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 100
     }
   });
   
