3b21ac62d337552bba14b9883471c7591339320b
Mark Choi <fantasysa@gmail.com>
2020-04-08 23:49:53 +0900

    Change background color and weather icon depending on the weather.

--- Weather.js
+++ Weather.js
@@ -4,18 +4,51 @@ import PropTypes from "prop-types";
 import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+
+const weatherOptions = {
+    Haze: {
+        iconName:"weather-windy",
+        bg: ["#6f6f90", "#758eb7"]
+    },
+    Clouds: {
+        iconName:"weather-cloudy",
+        bg: ["#7facd6", "#bfb8da"]
+    },
+    Thunderstorm: {
+        iconName:"weather-lightning",
+        bg: ["#4c669f", "#192f6a"]
+    },
+    Rain: {
+        iconName:"weather-rainy",
+        bg: ["#7c8cbf", "#bac94a"]
+    },
+    Snow: {
+        iconName:"weather-snowy",
+        bg: ["#47cacc", "#63bcc9"]
+    },
+    Clear: {
+        iconName:"weather-sunny",
+        bg: ["#f8d90f", "#fe7a15"]
+    },
+    Mist: {
+        iconName:"weather-fog",
+        bg: ["#428cd4", "#ff9cda"]
+    }
+}
+
 export default function Weather({temp, condition, city}) {
     return (
-    <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]} style={styles.container}>
+    <LinearGradient colors={weatherOptions[condition].bg} style={styles.container}>
         <StatusBar barStyle="light-content"/>
         <View style={styles.halfContainer}>
-            <MaterialCommunityIcons name="cloud" size={86} color="white"></MaterialCommunityIcons>
+            <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={86} color="white"></MaterialCommunityIcons>
             <Text style={styles.temp}>{temp}°</Text>
             
         </View>
         <View style={styles.halfContainer}>
-            <Text>{condition}</Text>
-            <Text>{city}</Text>
+            <Text style={styles.details}>{condition}</Text>
+            <Text style={styles.details}>{city} </Text>
+            <Text>test</Text>
         </View>
     </LinearGradient>
     );
@@ -23,7 +56,7 @@ export default function Weather({temp, condition, city}) {
 
 Weather.PropTypes = {
     temp: PropTypes.number.isRequired,
-    condition: PropTypes.oneOf(["Thunderstorm", "Drizzle", "Rain", "Snow", "Atomsphere", "Clear", "Clouds", "Haze", "Mist"]).isRequired,
+    condition: PropTypes.oneOf(["Thunderstorm", "Drizzle", "Rain", "Snow", "Clear", "Clouds", "Haze", "Mist"]).isRequired,
     city: PropTypes.string
 };
 
@@ -35,6 +68,10 @@ const styles=StyleSheet.create({
     },
     temp: {
         fontSize: 42,
+        color: "white"
+    },
+    details: {
+        fontSize: 20,
         color: "white"
     },
     halfContainer: {
